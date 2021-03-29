b6683f3fd1eb2d398d7869ce0b854050634027c9
Layan <layanjayasinghe2@gmail.com>
2021-03-30 00:33:17 +0530

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,16 @@
 import React from "react"
+import brand from "../images/brand.svg"
 
 const Navbar = () => {
-  return <div>navbar</div>
+  return (
+    <div className="relative">
+      <div className="py-4 w-full rounded-b-3xl lg:mx-5 md:mx-3 mx-2">
+        <nav className="flex justify-center items-center">
+          <img className="md:h-14 h-10" src={brand} alt="brand logo" />
+        </nav>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
