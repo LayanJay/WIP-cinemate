b746f4c4e3dfa28acacfdc6cd184865ed018d34a
Layan <layanjayasinghe2@gmail.com>
2021-04-11 22:04:25 +0530

    minor changes

--- src/templates/nowPlaying.jsx
+++ src/templates/nowPlaying.jsx
@@ -1,5 +1,5 @@
 import { graphql, Link } from "gatsby"
-import React, { useState, useEffect } from "react"
+import React from "react"
 import Layout from "../components/layout"
 import Header from "../components/header"
 import SEO from "../components/seo"
