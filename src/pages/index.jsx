ed6c595703ae304dfbf2f683c74edc5118fab939
Layan Jayasinghe <layanjayasinghe2@gmail.com>
2021-06-12 21:20:13 +0530

    migrate from gatsby to next

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,11 @@
-import * as React from 'react'
+import React from 'react'
 
-import Layout from '../components/layout'
-import Header from '../components/header'
-import SEO from '../components/seo'
-import NowPlaying from '../components/NowPlaying'
+import Layout from '../components/Layout'
 
 const IndexPage = () => {
   return (
     <Layout>
-      <SEO title="Home" />
-      <Header header />
-      <section className="lg:mx-5 md:mx-3 mx-2">
-        <NowPlaying />
-      </section>
+      <h1>hello</h1>
     </Layout>
   )
 }
